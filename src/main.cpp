534288c58e3e612da5e823cc80a789f4384e8a49
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-01-20 13:55:10 +0300

    21/01/2024

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char *argv[]) {
        /*TODO: разработать логику для hw-006*/ 
     std::cout << "hw-006" << std::endl;
         /*FIXME: Удалить неиспользуемые элементы*/
+        
     
 
     return 0;
